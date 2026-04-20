bf044aa055fa8beae7eff57e5c246312697fb072
Antonio Ortiz <ortiz.antonio@gmail.com>
2026-04-20 12:06:07 -0500

    modified font sizes

--- core/data/utopia/font-sizes.js
+++ core/data/utopia/font-sizes.js
@@ -6,7 +6,7 @@ export const fontConfig = {
   minWidth: Number.parseInt(token.breakpoints.sm.$value),
   maxWidth: Number.parseInt(token.breakpoints.lg.$value),
   minFontSize: 16,
-  maxFontSize: 20,
+  maxFontSize: 24,
   minTypeScale: 1.2,
   maxTypeScale: 1.25,
   positiveSteps: 4,
